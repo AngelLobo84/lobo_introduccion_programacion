28362fa9cdd354ea6d80c9364aa6651643c73ec0
AngelLobo84 <I202518008@CIBERTEC.EDU.PE>
2025-09-23 03:20:36 +0000

    Ejercicio de notas

--- 05_app.py
+++ 05_app.py
@@ -7,6 +7,8 @@ nota_ef = 13
 
 # Proceso: Definir el proceso de cálculo del promedio de notas
 # T1 (20%) + T2 (35%) + EF (45%)
+promedio_nota = (nota_t1 * 0.2 + nota_t2 * 0.35 + nota_ef * 0.45)
 
 
 # Salida: Mostrar el resultado del promedio
+print(f"El promedio de nota es: {promedio_nota}")
