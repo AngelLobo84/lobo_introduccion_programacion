ecc452a26ad90e9bce14e57b2b24a504bfa03185
AngelLobo84 <I202518008@CIBERTEC.EDU.PE>
2025-09-26 03:16:32 +0000

    ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+# Calcular monto de compra de equipo informatico
+
+# Definiendo variables
+nombre_producto = "PC Lenovo"
+Precio_unitario = 1500
+cantidad = 5
+
+# Cálculo de pedido de 5und
+precio_total = Precio_unitario * cantidad
+
+
+# Salida: calculo del precio total de los productos
+print(f"Nombre del producto: {nombre_producto}")
+print(f"Cantidad: {cantidad}")
+print(f"El precio total: {precio_total}")
