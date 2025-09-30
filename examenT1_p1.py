8874c611fee13efd4ae9a8f3a41a4d42f05f53e5
AngelLobo84 <I202518008@CIBERTEC.EDU.PE>
2025-09-30 03:52:31 +0000

    examen T1 pregunta 1

---
+++ examenT1_p1.py
@@ -0,0 +1,12 @@
+# varieble que recibe el monto en soles
+soles = input("Ingresa el monto en soles: ")
+# variable que recibe la tasa de cambio dolares
+dolar = input("ingresa tasa de cambio actual:")
+
+
+# Operación de cálculo de soles a dolares:
+cambio_dolar = int(soles) / float(dolar)
+print ()
+print (f"monto ingresado en soles es: {soles}")
+print (f"tipo de cambio ingresado es: {dolar}")
+print (f"el cambio de soles a dolares es: {cambio_dolar:.2f}")
